cc90401297d78bc8ff2ab935cdbea755f8c51c9b
sambaoumylaye <sambaoumylaye07@gmail.com>
2024-05-09 01:58:35 +0200

    Creation du projet

---
+++ AppSharedMemory/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppSharedMemory
+{
+    public partial class Form1 : Form
+    {
+        ServiceMetier.Service1Client servise;
+        public Form1()
+        {
+            servise = new ServiceMetier.Service1Client();
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
